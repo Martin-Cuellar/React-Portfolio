89ca37255c518d64483a904fd0b80f243742f53a
Martin Cuellar <martinhc129@gmail.com>
2023-05-02 18:56:03 -0400

    added css

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
+import './App.css';
 
 const App = () => {
   return (
