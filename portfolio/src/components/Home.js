1d86885102fb7898b2b95e372cba44167eee55fb
Martin Cuellar <martinhc129@gmail.com>
2023-05-02 18:23:35 -0400

    home.js

---
+++ portfolio/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <section id="home">
+      <h1>Martin Cuellar</h1>
+      <p>junior web developer.</p>
+    </section>
+  );
+};
+
+export default Home;
